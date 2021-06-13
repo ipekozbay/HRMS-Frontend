ecada84b13dd91f40fa5382bad4846545c72f398
İpek Özbay <ipek.ozbay@bil.omu.edu.tr>
2021-06-13 12:12:51 +0300

    Update SignedIn.jsx

--- src/layouts/SignedIn.jsx
+++ src/layouts/SignedIn.jsx
@@ -5,7 +5,7 @@ export default function SignedIn() {
     return (
         <div>
             <Menu.Item>
-                <Image avatar spaced="right" src="https://www.google.com/search?q=btk+akademi&biw=1197&bih=1158&tbm=isch&sxsrf=ALeKk00lwDMlLLZrg4V_xo-bnuMyDnd-fQ:1623509858504&source=lnms&sa=X&ved=2ahUKEwie3J7DrZLxAhUPglwKHbrPA0sQ_AUoA3oECAEQBQ#imgrc=8iOc6XvtljiCHM" />
+                <Image avatar spaced="right" src="link" />
                 <Dropdown>
                     <Dropdown.Menu>
                         <Dropdown.Item text="bilgilerim " icon="info" />
